c99bde9cabcd7418f6bb4c5a12f5d750be7c8cde
rodrigoap2 <32605497+rodrigoap2@users.noreply.github.com>
2017-12-02 19:33:22 -0200

    Add files via upload

--- satSolver.js
+++ satSolver.js
@@ -3,18 +3,26 @@ function readFormula(fileName) {
     var data = fs.readFileSync(fileName).toString();
     var text = data.split('\n');
     var getLine = " ";
-    for (i = 0;i < text.length;i++){
-        if (text[i].charAt(0) == 'p'){
+    for (i = 0; i < text.length; i++) {
+        if (text[i].charAt(0) == 'p') {
             getLine = text[i].split(" ");
         }
     }
-    var clauses = readClauses(text);
-    console.log(clauses);
-    var variables = readVariables(clauses);
+        var clauses = readClauses(text);
+        var variables = readVariables(clauses);
+        console.log(variables);
+        var specOk = checkProblemSpecification(text, clauses, variables);
+        var result = { 'clauses': [], 'variables': [] };
+         if (specOk) {
+            result.clauses = clauses;
+            result.variables = variables;
+         }
+        return result;
 }
+
 function readClauses(text) {
     var getLine = " ";
-    for (i = 0;i < text.length;i++){
+    for (i = 0; i < text.length; i++) {
         if (text[i].charAt(0) == 'p') {
             getLine = text[i].split(" ");
         }
@@ -22,13 +30,13 @@ function readClauses(text) {
     var array1 = [];
     var array2 = [];
     var getLine2 = "";
-    for (i = 0; i < text.length; i++){
-        if((text[i].charAt(0) != 'p') && (text[i].charAt(0) != 'c')){
+    for (i = 0; i < text.length; i++) {
+        if ((text[i].charAt(0) != 'p') && (text[i].charAt(0) != 'c')) {
             getLine2 = text[i].split(" ");
-            for (k = 0; k < getLine2.length; k++){
-                if(getLine2[k] != 0){
+            for (k = 0; k < getLine2.length; k++) {
+                if (getLine2[k] != 0) {
                     array2.push(getLine2[k]);
-                }else{
+                } else {
                     array1.push(array2);
                     array2 = [];
                 }
@@ -37,7 +45,42 @@ function readClauses(text) {
     }
     return array1;
 }
-function readVariables(clause) {
 
+function readVariables(clause) {
+    var biggestLength = 0;
+    for (i = 0; i < clause.length; i++) {
+        for (k = 0; k < clause[i].length; k++) {
+            if (Math.abs(clause[i][k]) > biggestLength) {
+                biggestLength = clause[i][k];
+            }
+        }
+    }
+    var variables = [];
+    for (i = 0; i < biggestLength; i++) {
+        variables.push(0);
+    }
+    return variables;
 }
-readFormula('hole1.cnf');
+
+function checkProblemSpecification(text, clauses, variables) {
+    var getLine;
+    specOk = true;
+    for (i = 0; i < text.length; i++) {
+        specOk = true;
+        if (text[i].charAt(0) == 'p') {
+            getLine = text[i].split(" ");
+        }
+    }
+    if (clauses.length != parseInt(getLine[3])) {
+        specOk = false;
+    } else if (variables.length != parseInt(getLine[2])) {
+        specOk = false;
+    }
+    return specOk;
+}
+
+function nextAssignment(currentAssignment) {
+    
+}
+
+console.log(readFormula('hole1.cnf'));
